f65bfe70d2b2088eb1c7ed48062054f5066cdbfc
lexus1999v-boop <lexus1999v@gmail.com>
2025-08-03 17:06:09 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const fetch = require("node-fetch");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.post("/generate", async (req, res) => {
